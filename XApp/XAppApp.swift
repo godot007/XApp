06ebeb4a7e9d9d9278f13199ffa6d27d4c71ba95
roy <clq3000@126.com>
2025-05-23 12:06:49 +0800

    Initial Commit

---
+++ XApp/XAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XAppApp.swift
+//  XApp
+//
+//  Created by Roy on 23/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct XAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
